da574cd4b764b22d158b538f907011b0c260648a
王玉针 <2919006783@qq.com>
2019-10-22 22:53:38 +0800

    Signed-off-by: 王玉针 <2919006783@qq.com>

--- WF/wf1.cpp
+++ WF/wf1.cpp
@@ -11,6 +11,11 @@ struct Letter
 	int num = 0;
 	char words;
 };
+struct words
+{
+	int n;
+	char c[30];
+}w[10000];
 
 Letter str[26];
 int sum = 0;
@@ -27,7 +32,6 @@ void countnum(FILE*fp)
 	int num = 0, ret;
 	while (!feof(fp))
 	{
-
 		letter = fgetc(fp);
 		//cout << letter << " " << endl;
 		if ((letter >= 'a'&&letter <= 'z') || (letter >= 'A'&&letter <= 'Z'))
@@ -40,10 +44,84 @@ void countnum(FILE*fp)
 	sort(str, str + 26, cmp);
 	return;
 }
+void countWord(FILE*fp)
+{
+	char b[30], ch;
+	int ij = 0, m = 1, j = 0, k = 0, t = 0, flag = 0;
+	while ((ch = fgetc(fp)) != EOF)
+	{
+		if ('A' <= ch&&ch <= 'Z')
+			ch = ch + 32;
+		if ('a' <= ch&&ch <= 'z')
+		{
+			b[ij] = ch;
+			ij++;
+			flag = 1;//开始写入b
+		}
+		else
+		{
+			if (ch == '-' && (ch = fgetc(fp)) == '\n')//非字母。非空格
+			{
+				flag = 0;
+			}
+			else
+			{
+				if (flag == 1)//空格
+				{
+					b[ij] = '\0';
+					ij = 0;
+					flag = 0;
+					m = 0;//写入b完成+'\0'
+					for (j = 0; j < k; j++)
+					{
+						if (strcmp(b, w[j].c) == 0)
+						{
+							m = 1;//标志  前有单词相同
+							break;
+						}
+					}
+					if (m)
+						w[j].n++;
+					else//存入结构体
+					{
+						w[k].n = 1;
+						strcpy(w[k].c, b);
+						k++;
+					}
+				}
+			}
+
+		}
+	}
+
+	/*输入 前5 */
+	printf("输出前5个最常出现的单词：");
+
+	for (ij = 0; ij < k&&ij < 5; ij++)
+	{
+		t = 0;
+		while (w[t].n == 0)
+			t++;
+		for (j = 1; j<k; j++)
+		{
+			if (w[j].n>w[t].n) t = j;//选 大的
+			else
+				if (w[j].n == w[t].n)
+				{
+					if (strcmp(w[j].c, w[t].c) < 0)
+						t = j;
+				}
+		}
+		printf("%s %d\n", w[t].c, w[t].n);
+		w[t].n = 0;//t 已输出，令n=0;
+	}
+
+}
 
 int main()
 {
 	FILE *fp;
+	fp = fopen("1.txt", "r");
 	int sl;
 	int allwords = 0;
 	if ((fp = fopen("1.txt", "r")) == NULL) {
@@ -56,6 +134,7 @@ int main()
 	//for (sum = 0; sum<26; sum++) {
 	//	str[sum].words = 'a' + sum;
 	countnum(fp);
+	countWord(fp);
 	//rewind(fp);
 	//}
 	fclose(fp);
@@ -65,7 +144,6 @@ int main()
 	}
 	for (int i = 0; i < 26; i++)
 	{
-		
 		printf("%c出现%d次,概率是：%.2f%%\n", str[i].words, str[i].num, (double)str[i].num / allwords);
 	}
 	cin >> sl;
